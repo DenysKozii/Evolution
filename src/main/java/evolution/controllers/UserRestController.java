89a91c78bbdb5c6bcee207f03700d3c75d7048a8
Den <denys.kozii@gmail.com>
2022-02-04 23:38:28 +0200

    cleanup code

--- src/main/java/evolution/controllers/UserRestController.java
+++ src/main/java/evolution/controllers/UserRestController.java
@@ -30,6 +30,9 @@ public class UserRestController {
 
     private UserServiceImpl userService;
     private JwtProvider jwtProvider;
+    private static final String GOOGLE_API = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=";
+    private static final String GIVEN_NAME = "given_name";
+    private static final String EMAIL = "email";
 
     @GetMapping("profile")
     public UserDto profile(@AuthenticationPrincipal UserDto user) {
@@ -42,9 +45,9 @@ public class UserRestController {
     }
 
     @PostMapping
-    public String googleLoginPost(@RequestBody String token) throws IOException {
+    public String googleLogin(@RequestBody String token) throws IOException {
         try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-            HttpUriRequest request = new HttpGet("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token);
+            HttpUriRequest request = new HttpGet(GOOGLE_API + token);
             HttpResponse response = client.execute(request);
             BufferedReader bufReader = new BufferedReader(new InputStreamReader(
                     response.getEntity().getContent()));
@@ -52,10 +55,10 @@ public class UserRestController {
             String username = "";
             String email = "";
             while ((line = bufReader.readLine()) != null) {
-                if (line.contains("given_name")) {
+                if (line.contains(GIVEN_NAME)) {
                     username = line.substring(17, line.length() - 2);
                 }
-                if (line.contains("email")) {
+                if (line.contains(EMAIL)) {
                     email = line.substring(12, line.length() - 2);
                 }
             }
@@ -65,7 +68,7 @@ public class UserRestController {
     }
 
     @PostMapping("login")
-    public String loginPost(@RequestParam String email, @RequestParam String username) {
+    public String login(@RequestParam String email, @RequestParam String username) {
         userService.register(email, username);
         return jwtProvider.generateToken(email);
     }
