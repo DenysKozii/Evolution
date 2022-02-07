46c40bb54d5b7670e1ea633798611f5eaead2e26
Denys Kozii <denys.kozii@gmail.com>
2022-02-07 18:08:13 +0200

    add abilities

--- src/main/java/evolution/enums/AbilityType.java
+++ src/main/java/evolution/enums/AbilityType.java
@@ -1,10 +1,25 @@
 package evolution.enums;
 
 public enum AbilityType {
-    DIVISION,
-    FANG,
-    HUNTING,
-    SHIELD,
-    DOUBLE_DIVISION,
+    SPEED_1,
+    DAMAGE_2,
+    HEALTH_3,
+    DIVISION_4,
+    RUN_5,
+    TEAM_6,
+    HUNT_7,
+    DAMAGE_FULL_8,
+    DAMAGE_9,
+    CHILD_HP_10,
+    GROW_11,
+    AGING_12,
+    HELP_13,
+    BIG_TEAM_14,
+    VAMPIRISM_15,
+    DIED_16,
+    STUN_17,
+    PROVOCATION_18,
+    CHILD_SPEED_19,
+    HELP_SPEED_20,
 
 }
--- src/main/java/evolution/services/impl/AbilityServiceImpl.java
+++ src/main/java/evolution/services/impl/AbilityServiceImpl.java
@@ -1,6 +1,13 @@
 package evolution.services.impl;
 
 import com.google.common.collect.ImmutableList;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import evolution.dto.AbilityDto;
 import evolution.dto.UserDto;
 import evolution.entity.Ability;
@@ -13,13 +20,7 @@ import evolution.repositories.UserRepository;
 import evolution.services.AbilityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,26 +45,44 @@ public class AbilityServiceImpl implements AbilityService {
         List<Ability> abilities = setupAbilitiesList();
         if (abilityRepository.count() != abilities.size()) {
             abilities.stream()
-                    .filter(ability -> !abilityRepository.existsByType(ability.getType()))
-                    .forEach(abilityRepository::save);
+                     .filter(ability -> !abilityRepository.existsByType(ability.getType()))
+                     .forEach(abilityRepository::save);
         }
     }
 
-    private List<Ability> setupAbilitiesList(){
-        Ability division = createAbility("Ускорить деление", "Уменьшает интервал деления с 6 до 4 секунд", 0, 0, AbilityType.DIVISION, ImmutableList.of());
-        Ability fang = createAbility("Растут клыки", "Выше урон", 0, 0, AbilityType.FANG, ImmutableList.of());
-        Ability shield = createAbility("Хитиновый панцирь", "Больше здоровья", 0, 0, AbilityType.SHIELD, ImmutableList.of());
-        Ability hunting = createAbility("Дальше распознает жертву", "Больше радиус распознавания", 0, 0, AbilityType.HUNTING, ImmutableList.of(fang));
-        Ability doubleDivision = createAbility("Двойное деление", "При делении появляется не 1, а 2 круга", 0, 0, AbilityType.DOUBLE_DIVISION, ImmutableList.of(division));
-        return Arrays.asList(division, fang, shield, hunting, doubleDivision);
+    private List<Ability> setupAbilitiesList() {
+        Ability SPEED_1 = createAbility("Увеличить скорость", "Увеличивает скорость передвижения", 0, 0, AbilityType.SPEED_1, ImmutableList.of());
+        Ability DAMAGE_2 = createAbility("Клыки", "Урон +20", 0, 0, AbilityType.DAMAGE_2, ImmutableList.of());
+        Ability HEALTH_3 = createAbility("Хитиновый панцирь", "Здоровье взрослого +50", 0, 0, AbilityType.HEALTH_3, ImmutableList.of());
+        Ability DIVISION_4 = createAbility("Уменьшить время деления", "Время деления -2 секунды", 0, 0, AbilityType.DIVISION_4, ImmutableList.of());
+        Ability RUN_5 = createAbility("Боязнь", "Если здоровья <20%, убегать", 0, 0, AbilityType.RUN_5, ImmutableList.of(SPEED_1));
+        Ability TEAM_6 = createAbility("Оберегать семью", "Ходить по парам", 0, 0, AbilityType.TEAM_6, ImmutableList.of(SPEED_1));
+        Ability HUNT_7 = createAbility("Охота", "Нападать на цель со здоровьем <50%", 0, 0, AbilityType.HUNT_7, ImmutableList.of(DAMAGE_2));
+        Ability DAMAGE_FULL_8 = createAbility("Ярость", "Урон +40, если >90% здоровья", 0, 0, AbilityType.DAMAGE_FULL_8, ImmutableList.of(DAMAGE_2));
+        Ability DAMAGE_9 = createAbility("Рога", "Урон +15, если >30% здоровья", 0, 0, AbilityType.DAMAGE_9, ImmutableList.of(HEALTH_3));
+        Ability CHILD_HP_10 = createAbility("Самостоятельность", "Здоровье детеныша +50", 0, 0, AbilityType.CHILD_HP_10, ImmutableList.of(HEALTH_3));
+        Ability GROW_11 = createAbility("Ускорить взросление", "Время взросления -3 секунды", 0, 0, AbilityType.GROW_11, ImmutableList.of(DIVISION_4));
+        Ability AGING_12 = createAbility("Увеличить Длительность жизни", "Время старения +10 секунд", 0, 0, AbilityType.AGING_12, ImmutableList.of(DIVISION_4));
+        Ability HELP_13 = createAbility("Альтруизм", "Бежать к раненому союзнику", 0, 0, AbilityType.HELP_13, ImmutableList.of(RUN_5, TEAM_6));
+        Ability BIG_TEAM_14 = createAbility("Командность", "Ходить по 3", 0, 0, AbilityType.BIG_TEAM_14, ImmutableList.of(TEAM_6));
+        Ability VAMPIRISM_15 = createAbility("Поедание добычи", "Получать 100 здоровья, если убил цель", 0, 0, AbilityType.VAMPIRISM_15, ImmutableList.of(HUNT_7, DAMAGE_FULL_8));
+        Ability DIED_16 = createAbility("Притворяться мертвым", "Притворяться мертвым на 3 секунды, когда здоровья <30%, восполнять 50", 0, 0, AbilityType.DIED_16, ImmutableList.of(DAMAGE_9, CHILD_HP_10));
+        Ability STUN_17 = createAbility("Массивные удары", "Если здоровья >200, каждый удар оглушает цель на 2 секунды", 150, 1, AbilityType.STUN_17, ImmutableList.of(CHILD_HP_10));
+        Ability PROVOCATION_18 = createAbility("Защищать детей", "Увеличить размер взрослых на 10, быть приоритетом для цели вместо детей на расстоянии 50", 50, 0, AbilityType.PROVOCATION_18, ImmutableList.of(GROW_11, AGING_12));
+        Ability CHILD_SPEED_19 = createAbility("Активный рост", "Время взросления -3 секунды, скорость детей +10", 10, 1, AbilityType.CHILD_SPEED_19, ImmutableList.of(GROW_11, AGING_12));
+        Ability HELP_SPEED_20 = createAbility("Помогать раненым", "Скорость передвижения к раненому союзнику +15", 150, 0, AbilityType.HELP_SPEED_20, ImmutableList.of(HELP_13));
+
+        return Arrays.asList(SPEED_1, DAMAGE_2, HEALTH_3, DIVISION_4, RUN_5, TEAM_6, HUNT_7, DAMAGE_FULL_8, DAMAGE_9,
+                             CHILD_HP_10, GROW_11, AGING_12, HELP_13, BIG_TEAM_14, VAMPIRISM_15, DIED_16, STUN_17,
+                             PROVOCATION_18, CHILD_SPEED_19, HELP_SPEED_20);
     }
 
-    private Ability createAbility(String title, String description, Integer coins, Integer crystals, AbilityType type, List<Ability> abilities) {
+    private Ability createAbility(String title, String description, Integer plasma, Integer dna, AbilityType type, List<Ability> abilities) {
         Ability newAbility = new Ability();
         newAbility.setTitle(title);
         newAbility.setDescription(description);
-        newAbility.setPlasma(coins);
-        newAbility.setDna(crystals);
+        newAbility.setPlasma(plasma);
+        newAbility.setDna(dna);
         newAbility.setType(type);
         newAbility.getConditionAbilities().addAll(abilities);
         abilityRepository.save(newAbility);
